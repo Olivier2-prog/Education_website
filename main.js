575925fc4d2c360758a4933a0de5fe303306a6ce
Olivis@123 <cyuzuzokwizeraolivier2@gmail.com>
2025-02-18 11:06:08 +0200

    Added js for menus

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
+const searchBtn = document.querySelector('.search-btn');
+const navToggler = document.querySelector(".fa-bars");
+const navBarList = document.querySelector(".nav-bar ul");
+const searchInput = document.querySelector("#search");
+/* Adding event listener */
+navToggler.addEventListener('click', function () {
+    navBarList.classList.toggle("show-nav");
+});
+searchBtn.addEventListener('click', function (e) {
+    e.preventDefault();
+    searchInput.classList.toggle("toggle-input");
+})
+window.addEventListener("scroll", function () {
+    navBarList.classList.remove("show-nav");   
+})
+/* End */
+
+
 
